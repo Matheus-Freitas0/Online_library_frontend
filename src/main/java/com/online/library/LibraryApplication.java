b59df4ffaab5ac531e3563d7e0f446393ef9c064
Matheus_Freitas0 <moncaomatheus@gmail.com>
2025-04-24 20:20:38 -0300

    stated project

---
+++ src/main/java/com/online/library/LibraryApplication.java
@@ -0,0 +1,14 @@
+package com.online.library;
+
+import org.flywaydb.core.internal.util.JsonUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
